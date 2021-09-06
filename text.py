3ccdd1bfbc4e743bd104981f79ec996e5fc20577
robiu402 <90174358+robiu402@users.noreply.github.com>
2021-09-06 12:09:25 +0600

    Add files via upload

---
+++ text.py
@@ -0,0 +1,32 @@
+red="\033[0;31m"   
+       green="\033[0;32m"        yellow="\033[0;33m"   
+    blue="\033[0;34m"    
+      purple="\033[0;35m"     
+  cyan="\033[0;36m"   
+
+       white="\033[0;37m
+                                                                                                                                                                                                   
+                                    bbbbbbbb                                                                                                                                                       
+RRRRRRRRRRRRRRRRR                   b::::::b              iiii                    lllllll                                                                                000000000       1111111   
+R::::::::::::::::R                  b::::::b             i::::i                   l:::::l                                                                              00:::::::::00    1::::::1   
+R::::::RRRRRR:::::R                 b::::::b              iiii                    l:::::l                                                                            00:::::::::::::00 1:::::::1   
+RR:::::R     R:::::R                 b:::::b                                      l:::::l                                                                           0:::::::000:::::::0111:::::1   
+  R::::R     R:::::R   ooooooooooo   b:::::bbbbbbbbb    iiiiiii uuuuuu    uuuuuu   l::::l         vvvvvvv           vvvvvvvaaaaaaaaaaaaa  uuuuuu    uuuuuu          0::::::0   0::::::0   1::::1   
+  R::::R     R:::::R oo:::::::::::oo b::::::::::::::bb  i:::::i u::::u    u::::u   l::::l          v:::::v         v:::::v a::::::::::::a u::::u    u::::u          0:::::0     0:::::0   1::::1   
+  R::::RRRRRR:::::R o:::::::::::::::ob::::::::::::::::b  i::::i u::::u    u::::u   l::::l           v:::::v       v:::::v  aaaaaaaaa:::::au::::u    u::::u          0:::::0     0:::::0   1::::1   
+  R:::::::::::::RR  o:::::ooooo:::::ob:::::bbbbb:::::::b i::::i u::::u    u::::u   l::::l            v:::::v     v:::::v            a::::au::::u    u::::u          0:::::0 000 0:::::0   1::::l   
+  R::::RRRRRR:::::R o::::o     o::::ob:::::b    b::::::b i::::i u::::u    u::::u   l::::l             v:::::v   v:::::v      aaaaaaa:::::au::::u    u::::u          0:::::0 000 0:::::0   1::::l   
+  R::::R     R:::::Ro::::o     o::::ob:::::b     b:::::b i::::i u::::u    u::::u   l::::l              v:::::v v:::::v     aa::::::::::::au::::u    u::::u          0:::::0     0:::::0   1::::l   
+  R::::R     R:::::Ro::::o     o::::ob:::::b     b:::::b i::::i u::::u    u::::u   l::::l               v:::::v:::::v     a::::aaaa::::::au::::u    u::::u          0:::::0     0:::::0   1::::l   
+  R::::R     R:::::Ro::::o     o::::ob:::::b     b:::::b i::::i u:::::uuuu:::::u   l::::l                v:::::::::v     a::::a    a:::::au:::::uuuu:::::u          0::::::0   0::::::0   1::::l   
+RR:::::R     R:::::Ro:::::ooooo:::::ob:::::bbbbbb::::::bi::::::iu:::::::::::::::uul::::::l                v:::::::v      a::::a    a:::::au:::::::::::::::uu        0:::::::000:::::::0111::::::111
+R::::::R     R:::::Ro:::::::::::::::ob::::::::::::::::b i::::::i u:::::::::::::::ul::::::l ......          v:::::v       a:::::aaaa::::::a u:::::::::::::::u ......  00:::::::::::::00 1::::::::::1
+R::::::R     R:::::R oo:::::::::::oo b:::::::::::::::b  i::::::i  uu::::::::uu:::ul::::::l .::::.           v:::v         a::::::::::aa:::a uu::::::::uu:::u .::::.    00:::::::::00   1::::::::::1
+RRRRRRRR     RRRRRRR   ooooooooooo   bbbbbbbbbbbbbbbb   iiiiiiii    uuuuuuuu  uuuullllllll ......            vvv           aaaaaaaaaa  aaaa   uuuuuuuu  uuuu ......      000000000     111111111111
+                                                                                                                                                                                                   
+                                                                                                                                                                                                   
+                                                                                                                                                                                                   
+                                                                                                                                                                                                   
+                                                                                                                                                                                                   
+                                                                                                                                                                                                   
+                                                                                                                                                                                                   
